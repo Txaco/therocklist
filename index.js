c974f7d7fb9cfa80f6c53fef4826539e57ca7b01
Txaco <42583806+Txaco@users.noreply.github.com>
2018-10-11 13:11:50 +0200

    Update and rename server.js to index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const port = 8080;
+const host = '/';
+
 const http = require('http');
 
 const server = http.createServer((request, response) => {
@@ -5,5 +8,11 @@ const server = http.createServer((request, response) => {
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.write('Node server is running at port ' + port + '. Everything is OK!');
   response.end();
+
+});
+
+server.listen(port, host, () => {
+
+  console.log('Node server is running at port ' + port + '. Everything is OK!');
 
 });
